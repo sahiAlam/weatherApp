0aff59a003227bb3d85530d58303c24f39e9f625
MD SAHI ALAM KHAN <ksahi4088@gmail.com>
2022-12-16 23:22:12 +0530

    another update

--- JS/script.js
+++ JS/script.js
@@ -1,60 +1,59 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'cb2b799f0cmsh9d8895e0c0907d7p14561ajsnace74d006225',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "cb2b799f0cmsh9d8895e0c0907d7p14561ajsnace74d006225",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
 };
 
 const getWeather = (city) => {
-    cityName.innerHTML = city;
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-        .then((response) => response.json())
-        .then((response) => {
-            console.log(response)
-    
-            cloud_pct.innerHTML = response.cloud_pct;
-            temp.innerHTML = response.temp;
-            temp2.innerHTML = response.temp;
-            feels_like.innerHTML = response.feels_like;
-            humidity.innerHTML = response.humidity;
-            humidity2.innerHTML = response.humidity;
-            min_temp.innerHTML = response.min_temp;
-            max_temp.innerHTML = response.max_temp;
-            wind_speed.innerHTML = response.wind_speed;
-            wind_speed2.innerHTML = response.wind_speed;
-            // wind_degrees.innerHTML = response.wind_degrees;
-            sunrise.innerHTML = response.sunrise;
-            sunset.innerHTML = response.sunset;
-        })
-        .catch((err) => console.error(err));
-}
+  cityName.innerHTML = city;
+  fetch(
+    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+
+      cloud_pct.innerHTML = response.cloud_pct;
+      temp.innerHTML = response.temp;
+      temp2.innerHTML = response.temp;
+      feels_like.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      humidity2.innerHTML = response.humidity;
+      min_temp.innerHTML = response.min_temp;
+      max_temp.innerHTML = response.max_temp;
+      wind_speed.innerHTML = response.wind_speed;
+      wind_speed2.innerHTML = response.wind_speed;
+      // wind_degrees.innerHTML = response.wind_degrees;
+      sunrise.innerHTML = response.sunrise;
+      sunset.innerHTML = response.sunset;
+    })
+    .catch((err) => console.error(err));
+};
 
 submit.addEventListener("click", (e) => {
-    e.preventDefault();
-    getWeather(inputCity.value)
-})
+  e.preventDefault();
+  getWeather(inputCity.value);
+});
 
-getWeather('Delhi');
+getWeather("Delhi");
+
+window.addEventListener("load", () => {
+  setTimeout(function () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+        document.getElementById("longi").innerHTML = position.coords.longitude;
+        document.getElementById("lati").innerHTML = position.coords.latitude;
+        // document.querySelector('.location1').style.display = "block";
+      });
+    } else {
+      console.log("Here Error!!");
+    }
+  }, 3000);
+});
 
 
-
-window.addEventListener('load', () => {
-    setInterval(
-        function() {
-            if(navigator.geolocation) {
-                navigator.geolocation.watchPosition((position) => {
-                    console.log(position.coords.longitude);
-                    console.log(position.coords.latitude);
-                    document.getElementById('longi').innerHTML = position.coords.longitude;
-                    document.getElementById('lati').innerHTML = position.coords.latitude;
-                    document.querySelectorAll('.location').style.display = "block";
-                    document.querySelectorAll('.location').style.display = "block";
-                });
-            } else {
-                console.log("Here Error!!")
-            }
-        },
-        4000
-    );
-})
